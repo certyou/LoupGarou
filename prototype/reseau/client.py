db0b4c65f9e4956c90dad3a7e5c8b48644ddbf39
Ewen <biduletheboss@gmail.com>
2024-10-03 09:29:48 +0200

    passage des fonctions en reseau en classe / WIP : le main

--- prototype/reseau/client.py
+++ prototype/reseau/client.py
@@ -1,16 +1,26 @@
 import socket
 
-def WithHostConnection() :
-    BroadcastRecv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    BroadcastRecv.bind(('', 65000))
-    Buffer = BroadcastRecv.recvfrom(1024)[0].decode()
-    HostIp = Buffer[1:Buffer.find(',')]
-    HostPort = int(Buffer[Buffer.find(',')+1:-1])
 
-    ToHostConnect = socket.socket()
-    ToHostConnect.connect((HostIp, HostPort))
-    return ToHostConnect
+class Client:
+    def __init__(self):
+        pass
+
+    def WithHostConnection() :
+        BroadcastRecv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        BroadcastRecv.bind(('', 65000))
+        Buffer = BroadcastRecv.recvfrom(1024)[0].decode()
+        HostIp = Buffer[1:Buffer.find(',')]
+        HostPort = int(Buffer[Buffer.find(',')+1:-1])
+
+        ToHostConnect = socket.socket()
+        ToHostConnect.connect((HostIp, HostPort))
+        print(f"Conneced to Host <--> {socket.getpeername()}")
+        return ToHostConnect
     
-
-socket = WithHostConnection()
-print(f"Conneced to Host <--> {socket.getpeername()}")
+    def WithIpConnection() :
+        Ip = input("Enter the Host IP: ")
+        Port = 50000 # port défini par convention
+        ToHostConnect = socket.socket()
+        ToHostConnect.connect((Ip, Port))
+        print(f"Conneced to Host <--> {socket.getpeername()}")
+        return ToHostConnect
