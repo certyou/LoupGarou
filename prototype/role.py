9e26d2f8a18e1525765060a2a7ad2dbdbcd15051
Noah <noahmahe56@gmail.com>
2024-10-09 16:03:16 +0200

    first commit test branch role

--- prototype/role.py
+++ prototype/role.py
@@ -5,9 +5,15 @@ class Wearwolf: # test commit
         self.name = "Loup garou"
         self.id = id
 
-    def ActionWearwolf(self):
-        choiseKillPlayer = PlayerChoice("Entrez le nom du joueur que vous shouaitez éliminer: ", )
-        return ChoiseKillPlayer.lower()
+    """
+    Input: int length (number of players in life)
+    Output: int choisePlayer (number of the player voted)
+    Action: Werewolves must choose one person to kill each night.
+    """
+    def ActionWearwolf(self, length):
+        expected_results = [i for i in range(1,length+1)]
+        choisePlayer = PlayerChoice("Entrez le numéro du joueur que vous shouaitez éliminer: ", expected_results)
+        return choisePlayer
 
 class Villager:
     def __init__(self, id):
@@ -15,4 +21,7 @@ class Villager:
         self.id = id
 
     def ActionVillager(self):
+        "A voir plus tard"
         return 0
+    
+
