ffb19f44c6a626a27fa7f7b8f8327c5c5aefe1a5
Lacroix Marceau <eaucram@gmail.com>
2024-10-09 16:04:34 +0200

    Création de la classe Hunter

--- prototype/role.py
+++ prototype/role.py
@@ -6,7 +6,7 @@ class Wearwolf: # test commit
         self.id = id
 
     def ActionWearwolf(self):
-        choiseKillPlayer = PlayerChoice("Entrez le nom du joueur que vous shouaitez éliminer: ", )
+        ChoiseKillPlayer = str(input("Entrez le nom du joueur que vous shouaitez éliminer: "))
         return ChoiseKillPlayer.lower()
 
 class Villager:
@@ -16,3 +16,27 @@ class Villager:
 
     def ActionVillager(self):
         return 0
+
+class Hunter:
+    def __init__(self, id):
+        self.name="Chasseur"
+        self.id = id
+    
+    def ActionHunter(self,lenght):
+
+        """Input : int lenght (number of player alive)
+           Output : int ChoiseKillPlayer (number of the player to kill)
+           Action : Ask the player to choose a player to kill when the Hunter dies"""
+        
+        prompt = "Entrez le numéro du joueur que vous souhaitez éliminer: "
+        expected_results = [str(i) for i in range(1,lenght+1)]
+        ChoiseKillPlayer = PlayerChoice(prompt, expected_results)
+
+        return ChoiseKillPlayer
+
+class Witch:
+    def __init__(self, id):
+        self.name="Sorcière"
+        self.id = id
+        self.Potion = True
+        self.Poison = True
