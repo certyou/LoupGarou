1add635a2ff14ef838e47ba8ff3abf6129ae3e92
Hector Hodebourg De Verbois <hodebourg-de-verbois.e2301128@etud.univ-ubs.fr>
2024-10-15 17:42:38 +0200

    new buffer useful function

--- prototype/useful_functions.py
+++ prototype/useful_functions.py
@@ -15,3 +15,20 @@ def PlayerChoice(prompt, expected_results):
         else:
             break
     return int(choice)
+
+def buffer(message) :
+    """
+    Function that identify the first frame of information inside the message
+    1 frame has to be like this : {type_of_return$str_message}
+    Arg : 
+        - : message : str, the message to analyze
+    Out :
+        - : strMessage : str, message that will be prompted to the player console
+        - : typeOfReturn : str, type of return the player has to give
+        - : message : str, message without the first frame"""
+    
+    typeOfReturn = message[message.find("{")+1: message.find("$")]
+    strMessage = message[message.find("$")+1: message.find("}")]
+    message = message[message.find("}")+1:]
+
+    return typeOfReturn, strMessage, message
