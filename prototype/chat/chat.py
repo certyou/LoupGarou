d9734094381a5cc70981cefe0823f22ef6d8048b
Hector Hodebourg De Verbois <hodebourg-de-verbois.e2301128@etud.univ-ubs.fr>
2024-11-06 15:37:15 +0100

    mise à niveau

--- prototype/chat/chat.py
+++ prototype/chat/chat.py
@@ -8,3 +8,4 @@ def print_hello():
 
 if __name__ == '__main__':
     print_hello()
+
--- prototype/chat/launcher.py
+++ prototype/chat/launcher.py
@@ -14,8 +14,8 @@ def open_new_console(script_name):
 def main() :
     script_path = os.path.join(os.path.dirname(__file__), 'chat.py')
     game_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'main_reseau.py')
-    open_new_console(script_path)  # Ouvrir la première console
-    open_new_console(game_path)  # Ouvrir la deuxième console
+    open_new_console(script_path)  # Chat py
+    #open_new_console(game_path)  # Ouvrir la deuxième console
 
 if __name__ == '__main__':
     main()
