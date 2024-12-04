fe9e42fbf3b8a4324d12dcda190698e38461f674
Lacroix Marceau <eaucram@gmail.com>
2024-12-04 17:13:39 +0100

    reload

--- prototype/main_reseau.py
+++ prototype/main_reseau.py
@@ -5,7 +5,7 @@ from game import Game
 from player import Player
 import useful_functions as utils
 from ascii_art import *
-from save import *
+import save as s
 import os
 import chat.launcher as launcher
 
@@ -22,8 +22,6 @@ def host():
     with open(os.path.join(os.path.dirname(__file__), "chat\\playerNumber.txt"), 'a', encoding='utf-8') as file:
             file.write(str(NbOfPlayers))
 
-    launcher.launchHostChat()
-
     choice=2
     with open("Save.json", "r") as file: # check if a save exist
         if len(file.read()) != 0:
@@ -39,64 +37,25 @@ def host():
 
         new_game = Game(ListOfPlayers)
         new_game.GameInit()
+        launcher.launchHostChat()
         new_game.GameLoop()
     else: # If the user wants to load a save we upload all the data from the save to recrate a game object from them
-        with open("Save.json", "r") as file:
-            data=json.load(file)
-        print("\nQuel sauvegarde voulez vous charger ? : ")
-        saveList=[str(i) for i in range (1,len(data.keys())+1) ]
-        saves=[]
-        cpt=0
-        for key in data.keys():
-            saves.append(key)
-            cpt+=1
-            print(f"{cpt} : {key}")
-        choice = int(utils.playerChoice("Votre choix : ", saveList))
-        save=load(saves[choice-1])
-        NbOfPlayers=len(save[0])
-        print("\nnombre de joueurs attendu :"+str(NbOfPlayers)+"\n")
-        GameHost = Host() # connection to the host
-        BroadcastThread = threading.Thread(target=GameHost.IPBroadcaster, args=(NbOfPlayers-1,), daemon=True)
-        BroadcastThread.start()
-        GameHost.TCPConnect(NbOfPlayers-1)
-        listOfPlayersSaved=save[0]
-        listOfPlayers=[]
-        for elem in listOfPlayersSaved: # we add the host as the first player of the list to not have conflict 
-            if elem.IsHost==True:
-                elem.id=None
-                elem.card.id=elem
-                listOfPlayersSaved.remove(elem)
-                listOfPlayers.append(elem)
-        name=[]
-        for elem in listOfPlayersSaved: # we get the name of every players to ask them wich one it was last time
-            name.append(elem.name)
-        nameExpected=[str(i) for i in range (1,len(name)+1)]
-        cpt=1
-        for i in range(len(listOfPlayersSaved)):
-            listOfPlayersSaved[i].id=GameHost.IPList[i]
-            savedNames = "\nLes différents noms de la dernière partie sont : \n"
-            for j in range(len(name)):
-                savedNames += f"{j+1} - {name[j]}\n"
-            utils.HostSendMessage(listOfPlayersSaved[i].id, savedNames, False)
-        for elem in listOfPlayersSaved: # we ask the name of the player to the player so we can associate the good player to the good role with the good id
-            namechoice=int(utils.playerChoice("\nQuel est votre nom de la dernière partie  ? :\n ", nameExpected , False, elem))
-            player=Player(elem.id, name[namechoice-cpt], False)
-            player.card=elem.card
-            listOfPlayers.append(player)
-            name.remove(name[namechoice-cpt])
-            cpt+=1
-
-        new_Game=Game(listOfPlayers)
-        new_Game.tabPlayerInLife=listOfPlayers
-        listOfRole=[]
+        reload = s.reloadGame()
+        listOfPlayers = reload[0]
+        new_Game = Game(listOfPlayers)
+        new_Game.tabPlayerInLife = listOfPlayers
+        listOfRole = []
         for elem in listOfPlayers:
-            if elem.name==save[1]:
-                new_Game.mayor=elem
-        listOfRole=[elem.card for elem in listOfPlayers]
-        new_Game.listOfRole=listOfRole
+            if elem.name == reload[1]:
+
+                new_Game.mayor = elem
+
+        listOfRole = [elem.card for elem in listOfPlayers]
+        new_Game.listOfRole = listOfRole
+        launcher.launchHostChat()
         print(new_Game.listOfRole)
-        new_Game.nbTurn=save[2]-1
-        new_Game.lovers=save[3]
+        new_Game.nbTurn = reload[2]-1
+        new_Game.lovers = reload[3]
         new_Game.GameLoop()
             
         
--- prototype/save.py
+++ prototype/save.py
@@ -2,6 +2,8 @@ import json
 from role import *
 from player import *
 from game import *
+from host import Host
+import threading
 
 def save(game,saveName):
     """Action : Save the date in the Save.json file as a dictionnary named saveName
@@ -73,5 +75,54 @@ def load(saveName):
     save.append(data[f"{saveName}"]["lovers"])
     file.close()
     return save
+
+def reloadGame():
+    with open("Save.json", "r") as file:
+        data=json.load(file)
+        print("\nQuel sauvegarde voulez vous charger ? : ")
+        saveList=[str(i) for i in range (1,len(data.keys())+1) ]
+        saves=[]
+        cpt=0
+        for key in data.keys():
+            saves.append(key)
+            cpt+=1
+            print(f"{cpt} : {key}")
+        choice = int(utils.playerChoice("Votre choix : ", saveList))
+        save=load(saves[choice-1])
+        NbOfPlayers=len(save[0])
+        print("\nnombre de joueurs attendu :"+str(NbOfPlayers)+"\n")
+        GameHost = Host() # connection to the host
+        BroadcastThread = threading.Thread(target=GameHost.IPBroadcaster, args=(NbOfPlayers-1,), daemon=True)
+        BroadcastThread.start()
+        GameHost.TCPConnect(NbOfPlayers-1)
+        listOfPlayersSaved=save[0]
+        listOfPlayers=[]
+        for elem in listOfPlayersSaved: # we add the host as the first player of the list to not have conflict 
+            if elem.IsHost==True:
+                elem.id=None
+                elem.card.id=elem
+                listOfPlayersSaved.remove(elem)
+                listOfPlayers.append(elem)
+        name=[]
+        for elem in listOfPlayersSaved: # we get the name of every players to ask them wich one it was last time
+            name.append(elem.name)
+        nameExpected=[str(i) for i in range (1,len(name)+1)]
+        cpt=1
+        for i in range(len(listOfPlayersSaved)):
+            listOfPlayersSaved[i].id=GameHost.IPList[i]
+            savedNames = "\nLes différents noms de la dernière partie sont : \n"
+            for j in range(len(name)):
+                savedNames += f"{j+1} - {name[j]}\n"
+            utils.HostSendMessage(listOfPlayersSaved[i].id, savedNames, False)
+        for elem in listOfPlayersSaved: # we ask the name of the player to the player so we can associate the good player to the good role with the good id
+            namechoice=int(utils.playerChoice("\nQuel est votre nom de la dernière partie  ? :\n ", nameExpected , False, elem))
+            player=Player(elem.id, name[namechoice-cpt], False)
+            player.card=elem.card
+            listOfPlayers.append(player)
+            name.remove(name[namechoice-cpt])
+            cpt+=1
+        return [listOfPlayers,save[1],save[2],save[3]]
+
+        
 
 #print(load("s1"))
