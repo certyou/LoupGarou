2df9f6db956096eb034d352ec16222182661f778
Lacroix Marceau <164542445+Eaucram@users.noreply.github.com>
2024-12-11 15:48:04 +0100

    Test save

--- prototype/save.py
+++ prototype/save.py
@@ -67,7 +67,10 @@ def load(saveName):
         elif elem["card"]["role"] == "Voleur":
             elem["card"]=Thief(elem["card"]["id"])
         player = Player(elem["id"],elem["name"],elem["IsHost"])
-        player.setRole(elem["card"])
+        if player.IsHost :
+            player.setRole(elem["card"])
+        else:
+            player.card=elem["card"]
         tabPlayerInLife.append(player)
     save.append(tabPlayerInLife) # We then recreate every attribute of Game that are important and add them to a list that will be returned
     save.append(data[f"{saveName}"]["mayor"])
